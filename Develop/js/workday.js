92b87c8d4af0d693a5870e26373ab770eab365e4
s.limguy <shuanmlim@icloud.com>
2022-03-13 13:49:23 -0500

    worked/ duplicated code for remainder of timeblocks

--- Develop/js/workday.js
+++ Develop/js/workday.js
@@ -6,7 +6,7 @@ $("#currentDay").html(todayDate);
 $(function () {
     // click event listener for saveBtn
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
+        // get values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         // save text/description within local storage
@@ -18,7 +18,7 @@ $(function () {
         // loop time blocks 
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-            // To check the time and add the classes for background indicators
+            // check time and add classes for background indicators
             if (blockTime < timeNow) {
                 $(this).addClass("past");
             }
@@ -35,6 +35,15 @@ $(function () {
     }
     // retrieve items from local storage 
     $("#hour8 .description").val(localStorage.getItem("hour8"));
-    
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    // run timeKeeper()
     timeKeeper();
 });
